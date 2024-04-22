30bc4255bfc75261e415c89a90b159bc59df4780
SimenBau <148766265+SimenBau@users.noreply.github.com>
2024-04-22 13:26:33 +0200

    I need more boolets

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-var randomTall = Math.round(Math.random()*100)
-console.log(randomTall);
+let tallInput = document.getElementById("tall");
+let sjekkButton = document.getElementById("check");
+let flaksButton = document.getElementById("random");
+let svar = Math.floor(Math.random() * 101);
+let attempts = 0;
  
-document.querySelector(".check").addEventListener("click", function () {
-    var mittTall = document.getElementsByClassName("searchbox").value;
-    if (mittTall == randomTall) {
-        document.querySelector(".result").innerHTML = "Gratulere. Du har funnet nummeret"
-    } else if (mittTall < randomTall) {
-        document.querySelector(".result").innerHTML = "Tallet er større"
-    } else if (mittTall > randomTall) {
-        document.querySelector(".result").innerHTML = "Tallet er mindre"
-    } 
-})
+sjekkButton.addEventListener("click", function() {
+  attempts++; 
+  let tall = parseInt(tallInput.value);
+ 
+  if (tall === svar) {
+    alert ("Riktig svar." + " Du brukte " + attempts + " forsøk.");
+    location.reload();
+    window.close();
+  } else if (tall < svar) {
+    alert("Skriv et tall større enn " + tall);
+  } else {
+    alert("Skriv et tall større enn " + tall);
+  }
+  console.log("Antall forsøk: " + attempts);
+});
+ 
+flaksButton.addEventListener("click", function() {
+  let flaks = Math.floor(Math.random() * 101);
+ 
+  if (flaks === svar) {
+    alert("Du fikk" + flaks + "\n" + " Du var heldig"  + " Du brukte " + attempts + " forsøk.");
+    location.reload();
+    window.close();
+  } else {
+    alert("Du fikk " + flaks + "\n" + " Du var ikke heldig");
+  }
+  attempts++;
+  console.log("Antall forsøk: " + attempts);
+});
+ 
+console.log("Svaret er: " + svar);
